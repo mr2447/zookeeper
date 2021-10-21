16a9deb2921a955efdb0d4e36bcb01cc6b605247
Marvin Ren <marvin.ren@yale.edu>
2021-10-20 22:24:15 -0400

    error

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
-app.listen(Port, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 })
